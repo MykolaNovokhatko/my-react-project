6f22186797b2712d0a730c57f164d518b14920c0
MykolaNovokhatko <143940513+MykolaNovokhatko@users.noreply.github.com>
2024-06-13 21:43:49 +0100

    test

---
+++ src/components/OfficerProfile.jsx
@@ -0,0 +1,7 @@
+export default function OfficerProfile(name, specialization, age) {
+    return <div>
+        <p>name: {name}</p>
+        <p>Age: {age}</p>
+        <p>specialization: {specialization} </p>
+    </div>;
+}
